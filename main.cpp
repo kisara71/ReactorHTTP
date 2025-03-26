83aee3b1634cb8c9b4763d5ed900d4d17cb6b7fb
kisara71 <2517167786@qq.com>
2025-03-26 21:03:32 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
-#include "Server/Server.h"
+//#include "Server/Server.h"
 #include <stdio.h>
 #include "Logger/Logger.h"
 #include "Config/Config.h"
 #include <unistd.h>
+#include "ThreadPool/ThreadPool.h"
+#include <thread>
+#include <iostream>
+#include <fcntl.h>
+#include <string.h>
 int main(int argc, char* argv[]){
 
-   Server ser;
+   //Server ser;
    Config config;
    config.parse_config(argc, argv);
-   Logger::get_instance()->log_init(config.logLevel, "/home/kisara/vscode/tinyWebserver/Logger/log.log");
-   ser.initServer(config.port);
+   Logger::get_instance()->log_init(config.logLevel, "./log.log");
+   //ser.initServer(config.port);
+   //ThreadPool tp;
+   {
+      ThreadPool(3);
+      getchar();
+   }
    getchar();
-    return 0;
+   return 0;
 }
