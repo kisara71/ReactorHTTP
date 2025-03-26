d3948407f2d2d0241a1f75e101299c9da6167cae
kisara71 <2517167786@qq.com>
2025-03-26 21:04:52 +0800

    增加回调函数

--- Channel/Channel.cpp
+++ Channel/Channel.cpp
@@ -1,13 +1,23 @@
 #include "Channel.h"
 #include <unistd.h>
-Channel::Channel():m_fd(-1), m_events(READEVENT) {
+#include <iostream>
+Channel::Channel(uint8_t events):
+m_fd(-1), 
+m_events(events),
+USELESS(false)
+{
 
 }
-
+void Channel::setUseless(bool flag)
+{
+    USELESS = flag;
+}
 Channel::~Channel(){
-    close(m_fd);
+    if(USELESS)
+    {
+        close(m_fd);
+    }
 }
-
 void Channel::setWriteEvent(bool flag){
     if(flag){
        m_events |= WRITEEVENT;
@@ -23,4 +33,15 @@ void Channel::setReadEvent(bool flag){
     }
 }
 
+void Channel::bindReadCallBack(std::function<bool(void)> func)
+{
+    readCallBack = std::move(func);
+    return;
+}
+
+void Channel::bindWriteCallBack(std::function<bool(void)> func)
+{
+    writeCallBack = std::move(func);
+    return;
+}
 
--- Channel/Channel.h
+++ Channel/Channel.h
@@ -1,25 +1,36 @@
 #ifndef CHANNEL_H
 #define CHANNEL_H
-enum Events{
+
+#include <functional>
+#include <cstdint>
+enum Events: uint8_t{
         TIMEOUT = 0x00,
         READEVENT = 0x01,
-        WRITEEVENT = 0x02
+        WRITEEVENT = 0x02,
+        UNSET = 0x04
 };
 
 class Channel{
 
 public:
-    Channel();
+    Channel(uint8_t events= 0x04);
     ~Channel();
 
 public:
     void setWriteEvent(bool flag);
     void setReadEvent(bool flag);
+    void setUseless(bool flag);
+    void bindReadCallBack(std::function<bool(void)> func);
+    void bindWriteCallBack(std::function<bool(void)> func);
+
+    std::function<bool(void)> readCallBack;
+    std::function<bool(void)> writeCallBack;
 public:
     
     int m_fd;
-    int m_events;
+    uint8_t m_events;
 private:
+    bool USELESS;
 
 };
 #endif
