a62fc9262da06b23c056fafe1fdbea8056f20cf8
kisara71 <2517167786@qq.com>
2025-05-03 14:37:03 +0800

    添加测试

--- main.cpp
+++ main.cpp
@@ -1,38 +1,19 @@
-//#include "Server/Server.h"
-#include <stdio.h>
-#include "Logger/Logger.h"
 #include "Config/Config.h"
-#include <unistd.h>
-#include "ThreadPool/ThreadPool.h"
-#include <thread>
-#include <iostream>
-#include <fcntl.h>
-#include <string.h>
+#include "Logger/Logger.h"
 #include "TCPServer/TCPServer.h"
+
 #include <thread>
 
-void test(TCPServer* ser)
-{
-   ser->run();
-}
+
+
 int main(int argc, char* argv[]){
 
-   //Server ser;
    Config config;
    config.parse_config(argc, argv);
    Logger::get_instance()->log_init(config.logLevel, "./log.log");
-   //ser.initServer(config.port);
-   //ThreadPool tp;
-   {
-      TCPServer* server = new TCPServer(1,55655, DispatcherType::EPOLL);
-      std::thread t(test, server);
-      getchar();
-      server->stop();
-      if(t.joinable())
-      {
-         t.join();
-      }
-   }
-   getchar();
+
+   TCPServer* server = new TCPServer(std::thread::hardware_concurrency(),55655, DispatcherType::EPOLL);
+   server->run();
+   
    return 0;
 }
